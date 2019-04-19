931efcc2ec1f4554df5a957b1dc866fcaf09b676
N-Ryo <ryo.joy.joy@gmail.com>
2019-04-19 11:28:59 +0900

    1st

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable
 
   def self.find_oauth(auth)
     uid = auth.uid
